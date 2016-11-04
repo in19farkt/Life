6773dc7594a39a0f434fb70966362870007292c1
Dmitriy Serdtsev <in19farkt@gmail.com>
2016-11-04 14:34:33 +0700

    Setup project

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+
+const webpack = require('webpack');
+
+module.exports = {
+    entry: [
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server',
+        './src/index.jsx'
+    ],
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loaders: [
+                "react-hot-loader/webpack",
+                "babel-loader"
+            ],
+        }]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    output: {
+        path: __dirname + '/public',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './public',
+        hot: true
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
+};
